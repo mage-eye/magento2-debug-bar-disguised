98b947b656c44763f72dae84764f76777b392407
Jerke Combee <jerke1988@gmail.com>
2024-09-25 20:17:18 +0200

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Copyright Jerke Combee. All rights reserved.
+ * https://www.jcombee.nl/
+ */
+
+\MageEye\MagentoDisguisedComponentRegistrar\Component\ComponentRegistrar::register(
+    \MageEye\MagentoDisguisedComponentRegistrar\Component\ComponentRegistrar::MODULE,
+    'MageEye_MagentoDebugBar',
+    __DIR__ . '/src'
+);
